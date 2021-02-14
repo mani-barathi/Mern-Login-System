9ecf1b9a7173ea610b48c86bc1685e8362450935
mani-barathi <manibarathi.s@gmail.com>
2021-02-14 21:32:30 +0530

    added loading spinner in Login Component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from 'react'
 function Login({ setUser }) {
     const [isLogin, setIsLogin] = useState(true)
     const [feedback, setFeedback] = useState({ message: '' })
+    const [isLoading, setIsLoading] = useState(true)
     const formRef = useRef()
 
     useEffect(() => {
@@ -16,12 +17,14 @@ function Login({ setUser }) {
                 console.log(data)
                 if (data.report)
                     setUser(data.user)
+                setIsLoading(false)
             }
             catch (error) {
                 console.log("ERROR:", error)
             }
         }
         makeRequest()
+        return () => { }
     }, [])
 
     const handleFormSubmit = async (event) => {
@@ -91,6 +94,7 @@ function Login({ setUser }) {
 
     // To clear Form Fields while user Switches between login and signup
     useEffect(() => {
+        if (isLoading) return
         setFeedback({ message: '' })
         clearFormFields()
     }, [isLogin])
@@ -99,72 +103,78 @@ function Login({ setUser }) {
         <div className="container ">
             <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
 
-                <form ref={formRef} onSubmit={handleFormSubmit}
-                    autoComplete="on" className="px-4 py-2 shadow-lg rounded border col-12" style={{ maxWidth: "500px" }} >
+                {!isLoading ? (
+                    <form ref={formRef} onSubmit={handleFormSubmit}
+                        autoComplete="on" className="px-4 py-2 shadow-lg rounded border col-12" style={{ maxWidth: "500px" }} >
 
-                    <h1 className="text-center mt-2 mb-2 font-weight-normal">
-                        {isLogin ? 'Login' : 'Sign Up'}
-                    </h1>
+                        <h1 className="text-center mt-2 mb-2 font-weight-normal">
+                            {isLogin ? 'Login' : 'Sign Up'}
+                        </h1>
 
-                    {!isLogin && /* User Name Field */
+                        {!isLogin && /* User Name Field */
+                            <div className="form-group">
+                                <label >Name</label>
+                                <input name="name" type="text" className="form-control" placeholder="Enter your Name" required />
+                            </div>
+                        }
+
                         <div className="form-group">
-                            <label >Name</label>
-                            <input name="name" type="text" className="form-control" placeholder="Enter your Name" required />
+                            <label >Email</label>
+                            <input name="email" type="email" className="form-control" placeholder="Enter your Email" required />
                         </div>
-                    }
 
-                    <div className="form-group">
-                        <label >Email</label>
-                        <input name="email" type="email" className="form-control" placeholder="Enter your Email" required />
-                    </div>
-
-                    <div className="form-group">
-                        <label >Password</label>
-                        <input name="password" type="password" className="form-control" placeholder="Enter your Password" minLength="6" required />
-                    </div>
-
-                    {!isLogin &&  /* Confirm Password Field */
                         <div className="form-group">
-                            <label >Confirm Password</label>
-                            <input name="confirmPassword" type="password" className="form-control" placeholder="Re-Enter your password" minLength="6" required />
+                            <label >Password</label>
+                            <input name="password" type="password" className="form-control" placeholder="Enter your Password" minLength="6" required />
                         </div>
-                    }
 
-                    {feedback.message && /* Error or Success Message */
+                        {!isLogin &&  /* Confirm Password Field */
+                            <div className="form-group">
+                                <label >Confirm Password</label>
+                                <input name="confirmPassword" type="password" className="form-control" placeholder="Re-Enter your password" minLength="6" required />
+                            </div>
+                        }
+
+                        {feedback.message && /* Error or Success Message */
+                            <div className="form-group">
+                                <h6 className={`text-center ${feedback.type === 1 ? 'text-success' : 'text-danger'}`}>
+                                    {feedback.message}
+                                </h6>
+                            </div>
+                        }
+
                         <div className="form-group">
-                            <h6 className={`text-center ${feedback.type === 1 ? 'text-success' : 'text-danger'}`}>
-                                {feedback.message}
+                            <button className="btn btn-primary btn-block">
+                                {isLogin ? 'Login' : 'Sign Up'}
+                            </button>
+                        </div>
+
+                        <div className="form-group">
+                            <h6 className="text-center">
+                                {isLogin ? (
+                                    <>
+                                        <span> New ? Create an Account</span>
+                                        <span className="text-primary ml-1" style={{ cursor: "pointer" }}
+                                            onClick={() => setIsLogin(false)}>
+                                            Here</span>
+                                    </>
+                                ) : (
+                                        <>
+                                            <span>Already have an Account?</span>
+                                            <span className="text-primary ml-1" style={{ cursor: "pointer" }}
+                                                onClick={() => setIsLogin(true)}>
+                                                Login</span>
+                                        </>
+                                    )}
                             </h6>
                         </div>
-                    }
 
-                    <div className="form-group">
-                        <button className="btn btn-primary btn-block">
-                            {isLogin ? 'Login' : 'Sign Up'}
-                        </button>
-                    </div>
-
-                    <div className="form-group">
-                        <h6 className="text-center">
-                            {isLogin ? (
-                                <>
-                                    <span> New ? Create an Account</span>
-                                    <span className="text-primary ml-1" style={{ cursor: "pointer" }}
-                                        onClick={() => setIsLogin(false)}>
-                                        Here</span>
-                                </>
-                            ) : (
-                                    <>
-                                        <span>Already have an Account?</span>
-                                        <span className="text-primary ml-1" style={{ cursor: "pointer" }}
-                                            onClick={() => setIsLogin(true)}>
-                                            Login</span>
-                                    </>
-                                )}
-                        </h6>
-                    </div>
-
-                </form>
+                    </form>
+                ) : (
+                        <div className="spinner-border text-primary" style={{ width: ' 5rem', height: '5rem' }} role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    )}
 
             </div>
         </div >
